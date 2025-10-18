46ebeb847524b905e893ed7bca9337ae2a78f4c1
InsomniaXD <tdrakecalilap@gmail.com>
2025-10-18 13:01:42 +0800

    Added a test button to gallery.html +  moved galler-audio.js to js folder

---
+++ src/js/gallery-audio.js
@@ -0,0 +1,30 @@
+// ========================================
+// ARTWORK DATA ARRAY
+// ========================================
+// This array contains all the artwork narrations that will be played in the gallery
+// Each artwork audio object contains: audio file path
+
+const artworkAudios = [
+        "kaganapan.m4a" // Audio file name  
+    ,
+        "transfiguration.m4a"
+    ,
+        "kiss_of_judas.m4a"
+    ,
+        "fredesvinda.m4a"
+    ,
+        "sandugo_monument.m4a"
+    ,
+        "the_crucifix_with_double_corpora.m4a"
+    ,
+        "altar_of_parish_of_the_holy_sacrifice.m4a"
+
+]
+
+// ========================================
+// PLAY ARTWORK AUDIO FUNCTION
+// ========================================
+
+function playArtworkAudioFromRoot(index) {
+    // JAKOB IDK ANYMORE
+}
